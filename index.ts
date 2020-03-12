825c5dede0cd18216215d2a2e08f7a1af1b29c0b
Rubén Palavecino <rubenpalavecino@localhost.localdomain>
2020-03-12 02:46:56 -0300

    automatico

--- index.ts
+++ index.ts
@@ -1,17 +1,19 @@
 import * as admin from 'firebase-admin';
-const coleccion = (colections:string[]):string[] => colections;
-const subcoleccion = (subcolections:string[]):string[] => subcolections;
 const firestore =  admin.firestore()
 const FieldValue = admin.firestore.FieldValue;
 const increment = FieldValue.increment(1);
 const decrement = FieldValue.increment(-1);
 
+let cole = ['']
+let subcole = ['']
+const coleccion = (colections:string[]):string[] => cole = colections;
+const subcoleccion = (subcolections:string[]):string[] => subcole = subcolections;
 
 
  const obtenerDatosDocumento = async(num:number,documento:string):Promise<any>=> {
     try{
         let doc = await firestore
-        .collection(`${coleccion[num]}`)
+        .collection(`${cole[num]}`)
         .doc(`${documento}`).get();
         let data = doc.data() as any;
         return data
@@ -21,19 +23,19 @@ const decrement = FieldValue.increment(-1);
     }
 };
 
-/*      OBTENER UNA COLECCION   */
-const obtenerDatosSubColeccion = async(num:number,documento:string,num2:number):Promise<any> => {
+/*      OBTENER UNA cole   */
+const obtenerDatosSubcole = async(num:number,documento:string,num2:number):Promise<any> => {
     try{
-        let datos_coleccion:[][] = []
+        let datos_cole:[][] = []
         let collection = await firestore
-        .collection(`${coleccion[num]}`)
+        .collection(`${cole[num]}`)
         .doc(`${documento}`)
-        .collection(`${subcoleccion[num2]}`).get();
+        .collection(`${subcole[num2]}`).get();
         collection.forEach(doc => {
             let data =  doc.data() as any;
-            datos_coleccion.push(data);
+            datos_cole.push(data);
         });  
-        return datos_coleccion 
+        return datos_cole 
     } 
     catch(err) { 
         console.error(err) 
@@ -44,7 +46,7 @@ const obtenerDatosSubColeccion = async(num:number,documento:string,num2:number):
 const agregarDocumento = async(num:number,documento:any):Promise<any> => {
     try{
         let agregado = await firestore
-        .collection(`${coleccion[num]}`)
+        .collection(`${cole[num]}`)
         .add(documento)
         return agregado;
     } catch(err) {
@@ -56,9 +58,9 @@ const agregarDocumento = async(num:number,documento:any):Promise<any> => {
 const agregarSubDocumento = async(num:number,documento:string,num2:number,subdocumento:any):Promise<any> => {
     try{
         let agregado = await firestore
-        .collection(`${coleccion[num]}`)
+        .collection(`${cole[num]}`)
         .doc(`${documento}`)
-        .collection(`${subcoleccion[num2]}`)
+        .collection(`${subcole[num2]}`)
         .add(subdocumento);
         return agregado;
     } catch(err) {
@@ -69,7 +71,7 @@ const agregarSubDocumento = async(num:number,documento:string,num2:number,subdoc
 const CrearDocumento = async(num:number,documento:string,campos:any):Promise<any> => {
     try{
         let agregado = await firestore
-        .collection(`${coleccion[num]}`)
+        .collection(`${cole[num]}`)
         .doc(`${documento}`)
         .set(campos);
         return agregado;
@@ -81,9 +83,9 @@ const CrearDocumento = async(num:number,documento:string,campos:any):Promise<any
 const CrearSubDocumento = async(num:number,documento:string,num2:number,subdocumento:string,documentocreado:any):Promise<any> => {
     try{
         let agregado = await firestore
-        .collection(`${coleccion[num]}`)
+        .collection(`${cole[num]}`)
         .doc(`${documento}`)
-        .collection(`${subcoleccion[num2]}`)
+        .collection(`${subcole[num2]}`)
         .doc(`${subdocumento}`)
         .set(documentocreado);
         return agregado;
@@ -96,7 +98,7 @@ const CrearSubDocumento = async(num:number,documento:string,num2:number,subdocum
 const actualizarDocumento = async(num:number,documento:string,actualizar:any):Promise<any> => { 
     try{
         await firestore
-        .collection(`${coleccion[num]}`)
+        .collection(`${cole[num]}`)
         .doc(`${documento}`)
         .update(actualizar);
         console.log('actualizado!')
@@ -107,9 +109,9 @@ const actualizarDocumento = async(num:number,documento:string,actualizar:any):Pr
 
 const actualizarSubDocumento = async(num:number,documento:string,num2:number,subdocumento:string,actualizar:any):Promise<void> => { 
     try{
-        await firestore.collection(`${coleccion[num]}`)
+        await firestore.collection(`${cole[num]}`)
         .doc(`${documento}`)
-        .collection(`${ subcoleccion[num2] }`)
+        .collection(`${ subcole[num2] }`)
         .doc(`${subdocumento}`)
         .update(actualizar);
         console.log('actualizado!')
@@ -120,7 +122,7 @@ const actualizarSubDocumento = async(num:number,documento:string,num2:number,sub
 
 const eliminarDocumento = async (num:number,uid:string) => {
     try {
-        await firestore.doc(`${coleccion[num]}/${uid}`).delete();
+        await firestore.doc(`${cole[num]}/${uid}`).delete();
     }   
 
     catch(err) { 
@@ -134,7 +136,7 @@ const eliminarDocumento = async (num:number,uid:string) => {
         try{
         
             firestore
-            .collection(`${coleccion[num]}`)
+            .collection(`${cole[num]}`)
             .doc(`${uid_proyecto}`)
             .update({ 
                 me_gusta_total: increment,
@@ -152,7 +154,7 @@ const noMeGustaDecrementar = async (num:number,uid_proyecto:string,user_uid:stri
     try{
     
         firestore
-        .collection(`${coleccion[num]}`)
+        .collection(`${cole[num]}`)
         .doc(`${uid_proyecto}`).update({ 
             me_gusta_total: decrement,
             users_likes:FieldValue.arrayRemove(user_uid)
@@ -164,8 +166,8 @@ const noMeGustaDecrementar = async (num:number,uid_proyecto:string,user_uid:stri
 }
 
 
-module.exports = coleccion;
-module.exports = subcoleccion;
+module.exports = cole;
+module.exports = subcole;
 module.exports = meGustaIncrementar;
 module.exports = noMeGustaDecrementar;
 module.exports = actualizarDocumento;
@@ -176,6 +178,6 @@ module.exports = CrearSubDocumento;
 module.exports = CrearDocumento;
 module.exports = eliminarDocumento;
 module.exports = obtenerDatosDocumento;
-module.exports = obtenerDatosSubColeccion;
-module.exports = coleccion;
-module.exports = subcoleccion;
+module.exports = obtenerDatosSubcole;
+module.exports = cole;
+module.exports = subcole;

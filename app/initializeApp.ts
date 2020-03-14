be6fbdef25502629902547f1baba01aeb02a0700
broker josef <brokereninversionesbitcoin@gmail.com>
2020-03-14 04:24:18 -0300

    automatico

---
+++ app/initializeApp.ts
@@ -0,0 +1,14 @@
+import * as admin from 'firebase-admin';
+
+export const initializeApp = (serviceAccount: string, databaseURL: string) => {
+    if (admin.apps.length === 0) {
+      admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: databaseURL
+      });
+      admin.firestore().settings({ timestampsInSnapshots: true });
+    }
+    return true;
+  };
+  
+  export { admin };
